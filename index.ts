5e2a6def0a89d51da7d506b5e4a9278a939bd5ef
masaa0802 <masaakigoto0802@gmail.com>
2022-09-25 15:55:24 +0900

    second commit

--- index.ts
+++ index.ts
@@ -1,2 +1,12 @@
-let hello : string = 'hello' ;
+import axios from 'axios';
+
+axios.get('http://google.com', {
+    headers:
+});
+
+let hello: string = 'hello';
 console.log(hello);
+
+function add(a: number, b:number): number {
+    return a + b;
+}
